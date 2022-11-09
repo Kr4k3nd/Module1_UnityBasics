aa75d98219cb26dafb796ab0d99714a9b64d7f3c
ACMClassUnity <alexcuquet@gmail.com>
2022-11-09 20:45:08 +0100

    AC_exercises7_20221109

--- Assets/Alexandra/Scripts/Ejercicios_6_10_AC/TargetMoveAC.cs
+++ Assets/Alexandra/Scripts/Ejercicios_6_10_AC/TargetMoveAC.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetMoveAC : MonoBehaviour
-{
-    public GameObject() waypoints;
-    int currentWP = 0;        //punto de inicio
-    public float speed = 7f;    //velocidad objeto
-
+{    
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +14,7 @@ public class TargetMoveAC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(Vector3.Distance(this.transform.position, waypoints(currentWP).transform.position) < 3)
-                currentWP++;
-
-    }if(currentWP >= waypoints.Length)
-        currentWP =0;
-    this.transform.LookAt(waypoints.[currentWP].transform);
-    this.transform.Translate(0,0,speed* Time.deltaTime);
+    }  
 
 
 }
