b2f3dd0305d0ede1c3b797d2c7780fc59ee58460
reokkotsu <quim.palau.shm@gmail.com>
2022-11-10 20:36:11 +0100

    ayudaarjona_qpalau

--- Assets/Quim/Ejercicio 7/Scripts/Target_qpalau.cs
+++ Assets/Quim/Ejercicio 7/Scripts/Target_qpalau.cs
@@ -5,29 +5,29 @@ using UnityEngine.UI;
 
 public class Target_qpalau : MonoBehaviour
 {
-    public GameObject TargetShoot;
-    public float cooldown = 1;
-    private float resetCooldown;
-    public Transform waypoint;
+    public GameObject bullet;    //gameobject que vamos spawnear
+    public float cooldown = 1;   //cuenta atrás
+    private float resetCooldown; //el valor que va a tener la cuenta atrás cuando reincie
+    public Transform waypoint;  //la posición del objeto desde donde va a spawnear la bullet
 
 
     // Start is called before the first frame update
     void Start()
     {
-        resetCooldown = cooldown;
+        resetCooldown = cooldown; //se guarda el valor para reiniciar la cuenta atrás más tarde 
     }
 
     // Update is called once per frame
     void Update()
     {
-        cooldown = cooldown - 1 * Time.deltaTime;
+        cooldown = cooldown - 1 * Time.deltaTime; //cada segundo que pasa resta una 
 
-        if (cooldown < 0)
+        if (cooldown < 0)  //si la cuenta es menor que
 
         {
 
-            Instantiate(TargetShoot, waypoint);
-            cooldown = resetCooldown;
+            Instantiate (bullet, waypoint); //spawnea una bala, en la posición de waypoint
+            cooldown = resetCooldown;  //reinicia la cuenta
         }
 
     }
